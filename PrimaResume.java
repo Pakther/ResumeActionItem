0cbc6040094b36a971a391713f0c92f3ae2fc773
Pakther <jannatul.ferdous.ssj@gmail.com>
2022-04-15 21:16:22 -0400

    Resume

---
+++ PrimaResume.java
@@ -0,0 +1,63 @@
+package com.company.qa.resume;
+
+public class PrimaResume {
+
+	public static void main(String[] args) {
+		String Name = "Prima Akther";
+		String Address = "234 Dahill Rd, Brooklyn NY 11218";
+		
+		/*
+		 * 
+		 * for phoneNum variable, what are other data types that can be used to iniate this object
+		 * 
+		 * 
+		 */
+		long phoneNum = 9290989876l;
+		String email = "pakther3322@gmail.com";
+		
+		String Skills = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n"
+				+ "Aenean ac interdum nisi. Sed in consequat mi.\n"
+				+ "";
+		
+		String Experience = "EXPERIENCE";
+		String ExpSecen1 = "Company Name, Location — Job Title\n"
+				+ "MONTH 20XX - PRESENT\n"
+				+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n"
+				+ "Aenean ac interdum nisi. Sed in consequat mi.\n"
+				+ "Sed in consequat mi, sed pulvinar\n"
+				+ "";
+		String ExpSecen2 = "Company Name, Location — Job Title\n"
+				+ "MONTH 20XX - PRESENT\n"
+				+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n"
+				+ "Aenean ac interdum nisi. Sed in consequat mi.\n"
+				+ "Sed in consequat mi, sed pulvinar\n"
+				+ "";
+		String ExpSecen3 = "Company Name, Location — Job Title\n"
+				+ "MONTH 20XX - PRESENT\n"
+				+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n"
+				+ "Aenean ac interdum nisi. Sed in consequat mi.\n"
+				+ "Sed in consequat mi, sed pulvinar\n"
+				+ "";
+		
+		
+	
+		String Education = "School Name, Location — Degree\n"
+				+ "MONTH 20XX - MONTH 20XX, LOCATION\n"
+				+ "";
+
+		System.out.println(Name);
+		System.out.println(Address);
+		System.out.println(phoneNum);
+		System.out.println(email);
+		System.out.println(Skills);
+		System.out.println(Experience);
+		System.out.println(ExpSecen1);
+		System.out.println(ExpSecen2);
+		System.out.println(ExpSecen3);
+		System.out.println(Education);
+
+		
+
+	}
+
+}
